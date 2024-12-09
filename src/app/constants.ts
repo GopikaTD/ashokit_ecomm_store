e914de861c314544b9b72c015ac5f493d9b9ca15
GopikaTD <gopikaniranjan18@gmail.com>
2024-12-09 13:30:21 +0530

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'http://a5a0029d2571e4f3495ab51500f19e55-1888408116.ap-south-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://af08dc73a293c4feb84df92fe3369892-21003783.ap-south-1.elb.amazonaws.com',
 };
